c29b6f1f15db1aab703a8d074195a2851ad2985e
cdelalama <cdelalama@gmail.com>
2022-02-11 09:50:54 +0100

    Ping server done

---
+++ features/ping.js
@@ -0,0 +1,25 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const dotenv_1 = __importDefault(require("dotenv"));
+const pingPort = Number(process.env.PING_PORT);
+dotenv_1.default.config();
+const server = (0, express_1.default)();
+server.get("/ping", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    res.send("pong");
+}));
+server.listen(pingPort, () => {
+    console.log("Server is ready...");
+});
